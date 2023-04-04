ef10a120654a4b4b137b4dc9a0f86145cea05bc0
Pryann <pryann@gmail.com>
2023-04-04 11:39:27 +0200

    update scripts

--- scripts.py
+++ scripts.py
@@ -3,9 +3,24 @@ import os
 
 def get_lessons_from_a_module(path):
     with open(path + '/order-list', 'w') as file:
-        for x in os.listdir(path):
-            if x.endswith('.py'):
-                file.write(x.replace('.py', '\n'))
+        for i in os.listdir(path):
+            if i.endswith('.py'):
+                file.write(i.replace('.py', '\n'))
 
 
-def order_module_lessons():
+def get_ordered_list(path):
+    with open(path + '/order-list', 'r') as file:
+        return file.read()
+
+
+def rename_module_lessons(path):
+    for i, v in enumerate(os.listdir(path)):
+        if v.endswith('.py'):
+            old_name = f'{path}/{v}'
+            new_name = f'{path}/{"%02d" % (i,)}-{v}'
+            print(old_name, new_name)
+
+
+path = './A list típus, vezérlési szerkezetek'
+ordered_list = get_ordered_list(path)
+rename_module_lessons(path)
