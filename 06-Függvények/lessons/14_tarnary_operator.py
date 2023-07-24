6d4af01bae5b3418fea536bc5c63dedc489d6eda
Pryann <pryann@gmail.com>
2023-07-24 21:17:54 +0200

    chg

--- 06-Függvények/lessons/14_tarnary_operator.py
+++ 06-Függvények/lessons/14_tarnary_operator.py
@@ -5,8 +5,6 @@ def is_even_number(num):
         return 'Odd'
 
 
-#
-#
 def is_even_number(num):
     if num % 2 == 0:
         return 'Even'
--- 06-Függvények/lessons/17_redundant_code_solution.py
+++ 06-Függvények/lessons/17_redundant_code_solution.py
@@ -7,5 +7,11 @@ def calcluate_high_salaries_sum(salaries, max_value):
 
 
 print(calcluate_high_salaries_sum([120000, 72000, 57000, 98000], 100000))
+print(calcluate_high_salaries_sum([100000, 172000, 157000, 91000], 120000))
 
+def calcluate_high_salaries_sum(salaries, max_value=100000):
+    return sum([salarie for salarie in salaries if salarie > max_value])
+
+
+print(calcluate_high_salaries_sum([120000, 72000, 57000, 98000]))
 print(calcluate_high_salaries_sum([100000, 172000, 157000, 91000], 120000))
